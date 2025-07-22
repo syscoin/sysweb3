76533328809b321fd52064a0f625b5c01ce44991
jagdeep sidhu <sidhujag@syscoin.org>
2025-07-22 14:25:54 -0700

    rm unused const

--- packages/sysweb3-keyring/src/ledger/consts.ts
+++ packages/sysweb3-keyring/src/ledger/consts.ts
@@ -27,4 +27,3 @@ export const BLOCKBOOK_API_URL = 'https://explorer-blockbook.syscoin.org';
 export const DESCRIPTOR = 'wpkh(@0/**)';
 export const RECEIVING_ADDRESS_INDEX = 0;
 export const WILL_NOT_DISPLAY = false;
-export const HD_PATH_STRING = `44'/60'/1'/0/`;
